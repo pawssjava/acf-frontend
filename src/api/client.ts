ef6e3dd2ab969d9222a5124dee48921233fe77ee
gazizdev <gaziz@java.kz>
2026-05-11 21:38:00 +0500

    init

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:9002';
+export const BASE_URL = import.meta.env.VITE_API_URL as string;
 
 export const publicClient = axios.create({ baseURL: BASE_URL });
 
